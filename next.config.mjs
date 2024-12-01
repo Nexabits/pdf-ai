17ee0138af21eea1f966206566e16f81143aa3a4
shamaskaramat <shams4195140@gmail.com>
2024-12-01 18:24:39 +0500

    ai pdf

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+
+export default nextConfig;
+
+
+
+// module.exports = {
+//     reactStrictMode: true,
+//     experimental: {
+//         reactRoot: true,
+//     },
+// };
